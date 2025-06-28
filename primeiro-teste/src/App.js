6cd2cebb9806367aef5ba9f06cd632fe85da61d7
ProfAndersonVanin <ProfAndersonVanin@gmail.com>
2025-06-28 13:02:18 -0300

    Interpolando com JSX

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var nome = "Anderson Vanin"
+
+  const url = 'https://placehold.co/150'
+
+  function somar(a,b){
+    return a+b
+  }
+
   return (
     <div className="App">
       <h1>Olá React</h1>
       <p>Meu Primeiro App React</p>
+      <p>O nome do prof é: {nome}</p>
+      <p>Soma {somar(1,2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
